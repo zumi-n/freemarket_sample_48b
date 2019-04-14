e6a72bf8297aeb3d5cfca5b7a48ca617992d294d
zumi-n <zumi0369@gmail.com>
2019-04-14 19:04:39 +0900

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/show' => 'users#show'
+  resources :users, only: [:show]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

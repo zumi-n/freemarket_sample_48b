d4596f814effb48693b135c237f80afae09e2eb6
kyohei-T <tomino@Tommy-MacBook-Pro.local>
2019-04-14 18:36:41 +0900

    指摘修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
-  get 'items/index'
   root 'items#index'
+  resources :items, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

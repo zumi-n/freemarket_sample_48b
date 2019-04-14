d82db4dc27279575bc6a14044621cf3e5559eb05
kyohei-T <tomino@Tommy-MacBook-Pro.local>
2019-04-14 16:49:38 +0900

    top-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
+  get 'items/index'
+  root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

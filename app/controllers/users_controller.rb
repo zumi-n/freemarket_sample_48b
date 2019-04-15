ebf88b99e00335506595f7b9731da94811480119
zumi-n <zumi0369@gmail.com>
2019-04-15 11:23:22 +0900

    Creat user-profile-edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
+
   def show
+    # @nickname = current_user.nickname
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to edit_user_path, flash.now[:notice] = "変更しました。"
+  end
+
+    private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
 end

239405a5648bb390fab56285aa66e8c8dca23311
zumi-n <zumi0369@gmail.com>
2019-04-15 14:38:40 +0900

    edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   resources :users, only: [:show, :edit]
-  resources :items, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

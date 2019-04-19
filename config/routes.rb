49c5108ebe06642dbf99fa04f8625debd796fc14
emincoring <emiri.a.812@gmail.com>
2019-04-19 13:15:21 +0900

    Add  listing item Views

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
     get :logout, on: :member
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'items/new'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_09_121150) do
+ActiveRecord::Schema.define(version: 2019_04_17_095927) do
+
+  create_table "places", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "prefecture_code"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false

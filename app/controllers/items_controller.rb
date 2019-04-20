96b72123528cf56131aa2968415ab1d2673853c7
kyohei-T <tomino@Tommy-MacBook-Pro.local>
2019-04-20 17:26:56 +0900

    items controller and model

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,25 @@
 class ItemsController < ApplicationController
   def new
+    @item = Item.new
+    @item.images.build
+    @item.build_delivery
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
   end
 
   def index
   end
 
   def show
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :desctiption, :brand, :size, :condition, :price, :profit, images_attributes:[:image], delivery_attributes:[:burden, :method, :area, :date])
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  has_one :delivery
-  has_many :comments
-  has_many :images
+  has_one :delivery, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :images, dependent: :destroy
+
+  accepts_nested_attributes_for :images, allow_destroy: true
+  accepts_nested_attributes_for :delibery, allow_destroy: true
 end

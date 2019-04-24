cea28ba66239aad1f58668dd991d22e3792c9356
zumi-n <zumi0369@gmail.com>
2019-04-24 13:08:48 +0900

    Edit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
 
+  before_action :set_item, only: [:show, :confirm]
+
   def index
     @items = Item.includes(:images).sample(4)
   end
@@ -8,12 +10,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
     @profile = Profile.find(params[:id])
     @delivery = Delivery.find(params[:id])
   end
 
   def confirm
+  end
+
+  def set_item
     @item = Item.find(params[:id])
   end
 

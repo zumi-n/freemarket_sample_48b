d674d8a9bf581aaf84564265e9fd1ccf8a2127a8
kyohei-T <tomino@Tommy-MacBook-Pro.local>
2019-04-25 19:29:24 +0900

    指摘修正

--- app/assets/javascripts/sell_item.js
+++ app/assets/javascripts/sell_item.js
@@ -70,23 +70,3 @@ $(function(){
     $("#item-sell-submit-btn").removeClass('disabled');
   })
 })
-
-// $(function(){
-//   $('#sell-item').on('submit', function(e){
-//     e.preventDefault();
-//     var formdata = new FormData(this);
-
-//     $.ajax({
-//       url: '/items',
-//       type: 'POST',
-//       data: formdata,
-//       dataType: 'json',
-//       processData: false,
-//       contentType: false
-//     }).done(function(){
-//       console.log("OK");
-//     }).fail(function(){
-//       console.log("NG");
-//     })
-//   })
-// })

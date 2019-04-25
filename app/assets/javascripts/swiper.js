b2e6f4851ff89828b5e2b94a6c1f2ecb1ef1463d
kyohei-T <tomino@Tommy-MacBook-Pro.local>
2019-04-26 05:04:08 +0900

    reference error

--- app/assets/javascripts/swiper.js
+++ app/assets/javascripts/swiper.js
@@ -8122,3 +8122,24 @@
   return Swiper;
 
 }));
+
+
+var mySwiper = new Swiper('.swiper-container', {
+  autoHeight: true,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    stopOnLastSlide: false,
+    disableOnInteraction: false,
+    reverseDirection: false
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  }
+});

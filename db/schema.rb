ba9ee95e911d67e497aa051fd91fcf3d781e1951
kyohei-T <tomino@Tommy-MacBook-Pro.local>
2019-04-26 11:47:24 +0900

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_23_055454) do
+ActiveRecord::Schema.define(version: 2019_04_26_024256) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -86,7 +86,7 @@ ActiveRecord::Schema.define(version: 2019_04_23_055454) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.string "name", null: false
     t.text "description"
     t.string "brand"

e62d647229cda997401b37eb3471b173427fca41
emincoring <emiri.a.812@gmail.com>
2019-04-27 14:47:29 +0900

    edit carousel files to resolve error

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,19 +1,19 @@
-var mySwiper = new Swiper('.swiper-container', {
-  autoHeight: true,
-  loop: true,
-  autoplay: {
-		delay: 3000,
-		stopOnLastSlide: false,
-		disableOnInteraction: false,
-		reverseDirection: false
-	},
-  navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev'
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    clickable: true
-  }
-});
+// var mySwiper = new Swiper('.swiper-container', {
+//   autoHeight: true,
+//   loop: true,
+//   autoplay: {
+// 		delay: 3000,
+// 		stopOnLastSlide: false,
+// 		disableOnInteraction: false,
+// 		reverseDirection: false
+// 	},
+//   navigation: {
+// 		nextEl: '.swiper-button-next',
+// 		prevEl: '.swiper-button-prev'
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     type: 'bullets',
+//     clickable: true
+//   }
+// });

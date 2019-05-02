005033550f7a4d657885e7eb8255d1cd160e9616
zumi-n <zumi0369@gmail.com>
2019-05-02 12:27:43 +0900

    Edit model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_29_073047) do
+ActiveRecord::Schema.define(version: 2019_05_02_022419) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false

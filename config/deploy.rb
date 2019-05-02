9af3047ca456ca6a1d4d079fd88616239facfed0
kyohei-T <tomino@Tommy-MacBook-Pro.local>
2019-05-02 21:04:54 +0900

    s3UPLOAD実装

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,8 @@ set :repo_url, "git@github.com:zumi-n/freemarket_sample_48b.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
+set :linked_files, %w[config/master.key]
+
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
@@ -21,6 +23,18 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'upload master.key'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/master.key', "#{shared_path}/config/master.key")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
 
 set :default_env, {

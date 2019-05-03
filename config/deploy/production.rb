148321dd84358997d6b896a371a0606bab8fd868
kyohei-T <tomino@Tommy-MacBook-Pro.local>
2019-05-03 17:05:09 +0900

    config deploy production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@ server '54.178.169.123', user: 'ec2-user', roles: %w{app db web}
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
 
-config.require_master_key = true
+#config.require_master_key = true
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
